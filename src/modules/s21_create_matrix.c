1936e7632ea88498de6ed8f99aada3080b137967
Cugerbol <cugerbol@student.21-school.ru>
2024-03-27 21:46:19 +0300

    update create_matrix

--- src/modules/s21_create_matrix.c
+++ src/modules/s21_create_matrix.c
@@ -1,5 +1,6 @@
 // by cugerbol
 #include "../s21_matrix.h"
+
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
   int error = OK;
   if (rows <= 0 || columns <= 0 || result == NULL)
@@ -10,6 +11,9 @@ int s21_create_matrix(int rows, int columns, matrix_t *result) {
     result->matrix = (double **)malloc(rows * sizeof(double *));
     for (int i = 0; i < rows; i++) {
       result->matrix[i] = (double *)malloc(columns * sizeof(double));
+      for (int j = 0; j < columns; j++) {
+        result->matrix[i][j] = 0.0;
+      }
     }
   }
   return error;
