e249de1d8d714dba515c33128b8c76036e4d319e
Cugerbol <cugerbol@student.21-school.ru>
2024-03-28 22:58:26 +0300

    update matrix_is_correct

--- src/modules/s21_secondary_func.c
+++ src/modules/s21_secondary_func.c
@@ -2,10 +2,13 @@
 #include "../s21_matrix.h"
 int matrix_is_correct(matrix_t *matrix) {
   int result = SUCCESS;
-  if (matrix == NULL || matrix->matrix == NULL || matrix->rows < 1 ||
-      matrix->columns < 1) {
+  if (matrix) {
+    if (matrix == NULL || matrix->matrix == NULL || matrix->rows < 1 ||
+        matrix->columns < 1) {
+      result = FAILURE;
+    }
+  } else
     result = FAILURE;
-  }
   return result;
 }
 
